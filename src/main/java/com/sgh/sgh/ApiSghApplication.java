418ab77e72a2c8d6e529495f075715f254a4d9aa
Carlos Rodas <crodasv2@miumg.edu.gt>
2024-08-06 22:19:10 -0600

    commit inicial

---
+++ src/main/java/com/sgh/sgh/ApiSghApplication.java
@@ -0,0 +1,13 @@
+package com.sgh.sgh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSghApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgh/sgh/ApiSghApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgh.sgh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
