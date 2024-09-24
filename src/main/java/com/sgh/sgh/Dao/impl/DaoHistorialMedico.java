b34364a6a44125a786da1ab504185c59ab3b55a5
victor lopez <vl40509@gmail.com>
2024-09-23 21:38:36 -0600

    Agregando microservicio de historial medico

---
+++ src/main/java/com/sgh/sgh/Dao/impl/DaoHistorialMedico.java
@@ -0,0 +1,41 @@
+package com.sgh.sgh.Dao.impl;
+
+import java.util.List;
+
+import com.sgh.sgh.Dao.IDaoHistorialMedico;
+import com.sgh.sgh.Entity.HistorialMedico;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+public class DaoHistorialMedico implements IDaoHistorialMedico {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	@Transactional
+	public List<HistorialMedico> listAll() {
+		return entityManager.createQuery("from HistorialMedico", HistorialMedico.class).getResultList();
+	}
+
+	@Override
+	@Transactional
+	public HistorialMedico findById(int id) {
+		return entityManager.find(HistorialMedico.class, id);
+	}
+
+	@Override
+	@Transactional
+	public HistorialMedico save(HistorialMedico historialMedico) {
+		entityManager.persist(historialMedico);
+		return historialMedico;
+	}
+
+	@Override
+	@Transactional
+	public HistorialMedico update(HistorialMedico historialMedico) {
+		entityManager.merge(historialMedico);
+		return historialMedico;
+	}
+}
